c7549c996259cf1ab4f946ebad1f4cdbf35c1ca0
Shirisha-Mangali <shirisha.mangali@cognine.com>
2025-11-11 11:05:12 +0530

    daily status

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
-const [count,setCount]=useState(0);
+
   return (
     <>
-      <h1>Counter Application</h1>
-      <p>count:{count}</p>
-      <button onClick={()=>{setCount(count+1)}}>Increment</button>
-      <button onClick={()=>{setCount(count-1)}}>Decrement</button>
+      <h1>Daily Status</h1>
+      
     </>
   )
 }
