6125a52393a93b24ede602af85587c03a670380f
Shirisha-Mangali <shirisha.mangali@cognine.com>
2025-11-11 10:27:08 +0530

    sencond commit after merge conflict

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+const [count,setCount]=useState(0);
+  return (
+    <>
+      <h1>Counter Application</h1>
+      <p>count:{count}</p>
+      <button onClick={()=>{setCount(count+1)}}>Increment</button>
+      <button onClick={()=>{setCount(count-1)}}>Decrement</button>
+    </>
+  )
+}
+
+export default App
