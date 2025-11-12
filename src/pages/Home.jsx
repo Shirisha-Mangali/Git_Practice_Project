ae77d5a9b0b9d062dc360cc09df721fdb74e9526
Shirisha-Mangali <shirisha.mangali@cognine.com>
2025-11-12 12:01:51 +0530

    git add .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+ 
 const Home =()=>{
     return(
         <>
-        <h1>Daily status Home</h1>
+        <div>
+            <h1>WelCome to my Learning log Portal</h1>
+            <div>
+                <p>Here are the Tasks that you have covered today </p>
+                <p>1. React app using vite</p>
+                <p>2. React Router Dom</p>
+                <p>3. Basic Routing</p>
+                <p>4. Nested Routing</p>
+            </div>
+        </div>
         </>
     )
 }
